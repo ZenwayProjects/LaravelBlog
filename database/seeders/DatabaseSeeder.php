9cd4be885e16b4ce45175c1d431b1e9a8a4e8746
ZenwayProjects <zenway15@gmail.com>
2021-02-20 19:51:32 -0500

    borrado del course seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,7 @@
 
 namespace Database\Seeders;
 
-use App\Models\Curso;
+use App\Models\Course;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -15,7 +15,33 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
-        $this->call(CourseSeeder::class);
+            Course::factory(50)->create();
+        //$this->call(CourseSeeder::class);
+        // $course = new Course();
+
+        // $course->name = "Laravel";
+        // $course->description = "Best PHP Framework";
+        // $course->category = "Web Development";
+
+        // $course->save();
+
+
+        // $course2 = new Course();
+
+        // $course2->name = "Vue.js";
+        // $course2->description = "A JavaScript Framework";
+        // $course2->category = "Web Development";
+
+        // $course2->save();
+
+
+        // $course3 = new Course();
+
+        // $course3->name = "React.js";
+        // $course3->description = "A JavaScript Library";
+        // $course3->category = "Web Development";
+
+        // $course3->save();
       
     }
 }
